f4b5ab04f8326c798a60ab54afad205e59374727
Praveen <pravee.spk.java@gmail.com>
2019-11-26 13:19:16 +0530

    Adding logs and Profiles for Dev and Prod

--- spk-rest-demo-d1/src/main/java/com/spk/rest/demo/UserController.java
+++ spk-rest-demo-d1/src/main/java/com/spk/rest/demo/UserController.java
@@ -1,6 +1,8 @@
 package com.spk.rest.demo;
 
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,28 +15,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+	static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
 	@Autowired
 	private UserService userService;
 
 	@GetMapping("/users")
 	public List<User> findAllUsers() {
-
+		
+		logger.info("Invoked findAllUsers Service");
+		logger.debug("Fetching all users");
 		return userService.findAll();
 	}
 
 	@PostMapping("/add")
 	public User add(@RequestBody User user) {
 
+		logger.info("Invoked add Service");
+		logger.debug("Adding user : " + user.getEmail());
 		userService.add(user);
-
 		return user;
 	}
 
 	@DeleteMapping("/delete/{id}")
 	public String delete(@PathVariable int id) {
 
+		logger.info("Invoked delete Service");
+		logger.debug("Deleting user with Id : " + id);
 		userService.deleteById(id);
-
 		return "Successfully Deleted User of Id : " + id;
 	}
 
--- spk-rest-demo-d1/src/main/java/com/spk/rest/demo/UserService.java
+++ spk-rest-demo-d1/src/main/java/com/spk/rest/demo/UserService.java
@@ -6,13 +6,19 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+	
+	static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
 	private static List<User> users = new ArrayList<>();
 	static {
+		
+		logger.info("Adding pre defined Users...........");
 		users.add(new User(101, "Aaa", LocalDate.of(1951, 1, 11), "aaa@spring.com", "9999999999"));
 		users.add(new User(102, "Bbb", LocalDate.of(1952, 2, 12), "bbb@spring.com", "8888888888"));
 		users.add(new User(103, "Ccc", LocalDate.of(1953, 3, 13), "ccc@spring.com", "7777777777"));
@@ -22,28 +28,34 @@ public class UserService {
 	}
 
 	public List<User> findAll() {
-
+		logger.info("Starting  findAll method");
+		logger.info("Completed  findAll method");
 		return users;
 	}
 
 	public void add(User user) {
+		logger.info("Starting  add method");
 		user.setId(generateRandomId());
-
 		users.add(user);
+		logger.info("Completed  add method");
 	}
 
 	private int generateRandomId() {
-
+		logger.debug("Starting  generateRandomId method");
+		logger.debug("Completed  generateRandomId method");
 		return new Random().nextInt(1000);
 	}
 
 	public List<User> deleteById(int id) {
+		logger.info("Starting  deleteById method");
 		users.removeIf(u -> u.getId() == id);
+		logger.info("Completed  deleteById method");
 		return findAll();
 	}
 
 	public Optional<User> find(int id) {
-
+		logger.debug("Starting  find method");
+		logger.debug("Completed  find method");
 		return users.stream().filter(u -> u.getId() == id).findFirst();
 	}
 
